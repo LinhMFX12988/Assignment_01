245762068a4addcf2d54a724dcf2ad6e845b9840
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2021-11-23 17:49:39 +0700

    first commit project 1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/StaffListComponent";
+import "./App.css";
+import { STAFFS } from './shared/staffs';
+import { DEPARTMENTS } from './shared/staffs';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: STAFFS
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Ứng dụng quản lí nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
+import dateFormat from 'dateformat'; 
+
+class Menu extends Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          selectedDish: null
+      }
+  }
+
+  onDishSelect(dish) {
+      this.setState({ selectedDish: dish});
+  }
+
+  renderDish(dish) {
+      if (dish != null)
+          return(
+                  <CardBody>
+                    <CardText><b>{'Họ và tên: ' + dish.name}</b></CardText>
+                    <CardText>{'Ngày sinh: ' + dish.doB}</CardText>
+                    <CardText>{'Ngày vào công ty: ' + dish.startDate}</CardText>
+                    <CardText>{'Phòng ban: '}</CardText>
+                    <CardText>{'Số ngày nghỉ còn lại: ' + dish.annualLeave}</CardText>
+                    <CardText>{'Số ngày đã làm thêm: ' + dish.overTime}</CardText>
+                  </CardBody>
+          );
+      else
+          return(
+              <div>Bấm vào tên nhân viên để xem thông tin</div>
+          );
+  }
+
+  render() {
+      const menu = this.props.dishes.map((dish) => {
+          return (
+            <div className="col-3 col-md-5 m-4">
+              <Card key={dish.id}
+                onClick={() => this.onDishSelect(dish)}>
+                
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+          );
+      });
+
+      return (
+          <div className="container">
+              <div className="row">
+                  {menu}
+              </div>
+              <div className="row">
+                <div  className="col-12 col-md-5 m-5">
+                  {this.renderDish(this.state.selectedDish)}
+                </div>
+              </div>
+          </div>
+      );
+  }
+}
+
+export default Menu;
