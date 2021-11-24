dee036ddb26884082c7317a8f168df09838a5cbd
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2021-11-24 16:56:45 +0700

    commit 4

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -12,21 +12,21 @@ class Menu extends Component {
       }
   }
 
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
+  onDishSelect(staffs) {
+      this.setState({ selectedDish: staffs});
   }
-  renderDish(dish) {      
-      if (dish != null)
+  renderDish(staffs) {      
+      if (staffs != null)
           return(
                   <CardBody>
-                    <h3>{'Họ và tên: ' + dish.name}</h3>
-                    <CardText>{'Ngày sinh: ' + dateFormat(dish.doB, "dd/mm/yyyy")}</CardText>
-                    <CardText>{'Ngày vào công ty: ' + dateFormat(dish.startDate, "dd/mm/yyyy")}</CardText>
-                    <CardText>{'Phòng ban: ' + dish.department.name}</CardText>
-                    <CardText>{'Số ngày nghỉ còn lại: ' + dish.annualLeave}</CardText>
-                    <CardText>{'Số ngày đã làm thêm: ' + dish.overTime}</CardText>
+                    <h3>{'Họ và tên: ' + staffs.name}</h3>
+                    <CardText>{'Ngày sinh: ' + dateFormat(staffs.doB, "dd/mm/yyyy")}</CardText>
+                    <CardText>{'Ngày vào công ty: ' + dateFormat(staffs.startDate, "dd/mm/yyyy")}</CardText>
+                    <CardText>{'Phòng ban: ' + staffs.department.name}</CardText>
+                    <CardText>{'Số ngày nghỉ còn lại: ' + staffs.annualLeave}</CardText>
+                    <CardText>{'Số ngày đã làm thêm: ' + staffs.overTime}</CardText>
                   </CardBody>
-          );
+          );  
       else
           return(
               <div>Bấm vào tên nhân viên để xem thông tin</div>
@@ -34,14 +34,14 @@ class Menu extends Component {
   }
   
   render() {
-      const menu = this.props.dishes.map((dish) => {
+      const menu = this.props.dishes.map((staffs) => {
           return (
             <div className="col-3 col-md-5 m-4">
-              <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+              <Card key={staffs.id}
+                onClick={() => this.onDishSelect(staffs)}>
                 
                 <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
+                    <CardTitle>{staffs.name}</CardTitle>
                 </CardImgOverlay>
               </Card>
             </div>
