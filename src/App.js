1bbd8b87622a1e2555672e2cc51abd1afa44add7
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2021-11-26 15:37:25 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -2,42 +2,52 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/StaffListComponent";
 import "./App.css";
-import { STAFFS } from './shared/staffs';
-import "./components/lightMode";
- 
-class App extends Component {
+import { STAFFS } from "./shared/staffs";
 
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: STAFFS,
-      darkMode:false,
+      darkMode: false,
     };
   }
   render() {
-    // const [darkMode, setDarkMode] = useState(false);
     return (
-      
-      <div className={this.state.darkMode ? "dark-mode" : "light-mode"} >
+      <div className={this.state.darkMode ? "dark-mode" : "light-mode"}>
         <Navbar dark color="primary">
-          <div className="container"> 
+          <div className="container">
             <NavbarBrand>Ứng dụng quản lí nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
         <div className={this.state.darkMode ? "dark-mode" : "light-mode"}>
-        <div className="container-2">
-        <Menu dishes={this.state.dishes} /> 
-          <span style={{ color: this.state.darkModedarkMode ? "grey" : "yellow" }}>☀︎</span>
-          <div className="switch-checkbox">
-            <label className="switch">
-              <input type="checkbox" onChange={() => this.setState({darkMode: !this.state.darkMode})} />
-              <span className="slider round"> </span>
-            </label>
+          <Menu dishes={this.state.dishes} />
+          <div className="container-2">
+            <span
+              style={{
+                color: this.state.darkMode ? "grey" : "yellow",
+              }}
+            >
+              ☀︎
+            </span>
+
+            <div className="switch-checkbox">
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  onChange={() =>
+                    this.setState({ darkMode: !this.state.darkMode })
+                  }
+                />
+                <span className="slider round"> </span>
+              </label>
+            </div>
+
+            <span style={{ color: this.state.darkMode ? "#c96dfd" : "grey" }}>
+              ☽
+            </span>
           </div>
-          <span style={{ color: this.state.darkMode ? "#c96dfd" : "grey" }}>☽</span>
-        </div>        
-      </div>    
-               
+        </div>
       </div>
     );
   }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
-import dateFormat from 'dateformat';
+import React, { Component } from "react";
+import { Card, CardText, CardBody, CardTitle } from "reactstrap";
+import dateFormat from "dateformat";
 
 class Menu extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      selectedDish: null
-    }
+      selectedDish: null,
+    };
   }
-
 
   onDishSelect(staffs) {
     this.setState({ selectedDish: staffs });
@@ -19,27 +17,26 @@ class Menu extends Component {
     if (staffs != null)
       return (
         <CardBody>
-          <h3>{'Họ và tên: ' + staffs.name}</h3>
-          <CardText>{'Ngày sinh: ' + dateFormat(staffs.doB, "dd/mm/yyyy")}</CardText>
-          <CardText>{'Ngày vào công ty: ' + dateFormat(staffs.startDate, "dd/mm/yyyy")}</CardText>
-          <CardText>{'Phòng ban: ' + staffs.department.name}</CardText>
-          <CardText>{'Số ngày nghỉ còn lại: ' + staffs.annualLeave}</CardText>
-          <CardText>{'Số ngày đã làm thêm: ' + staffs.overTime}</CardText>
+          <h3>{"Họ và tên: " + staffs.name}</h3>
+          <CardText>
+            {"Ngày sinh: " + dateFormat(staffs.doB, "dd/mm/yyyy")}
+          </CardText>
+          <CardText>
+            {"Ngày vào công ty: " + dateFormat(staffs.startDate, "dd/mm/yyyy")}
+          </CardText>
+          <CardText>{"Phòng ban: " + staffs.department.name}</CardText>
+          <CardText>{"Số ngày nghỉ còn lại: " + staffs.annualLeave}</CardText>
+          <CardText>{"Số ngày đã làm thêm: " + staffs.overTime}</CardText>
         </CardBody>
       );
-    else
-      return (
-        <div>Bấm vào tên nhân viên để xem thông tin</div>
-      );
+    else return <div>Bấm vào tên nhân viên để xem thông tin</div>;
   }
 
   render() {
-   
     const menu = this.props.dishes.map((staffs) => {
       return (
         <div key={staffs.id} className="col- col-md-6 col-xl-4">
-          <Card
-            onClick={() => this.onDishSelect(staffs)}>
+          <Card onClick={() => this.onDishSelect(staffs)}>
             <CardTitle>{staffs.name}</CardTitle>
           </Card>
         </div>
@@ -47,21 +44,16 @@ class Menu extends Component {
     });
 
     return (
-      
-      <div className="container">            
-        <div className="row">
-          {menu}
-        </div>
+      <div className="container">
+        <div className="row">{menu}</div>
         <div className="row">
           <div className="col-12 col-md-12 col-xl-12">
             {this.renderDish(this.state.selectedDish)}
           </div>
         </div>
       </div>
-      
     );
   }
 }
 
 export default Menu;
-
